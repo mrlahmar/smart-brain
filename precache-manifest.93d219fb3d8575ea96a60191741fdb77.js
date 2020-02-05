63a2068aca811d0db50bc110de80908feb9c2e00
Alaa Lahmar <lahmar11ala@gmail.com>
2020-02-05 12:54:20 +0100

    Updates

---
+++ precache-manifest.93d219fb3d8575ea96a60191741fdb77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ef641d904f51320dd73ea6367e7619",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "a81e87970a5c36defcfe",
+    "url": "/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "703f63b4d5f87cdd2f28",
+    "url": "/smart-brain/static/css/main.5562f846.chunk.css"
+  },
+  {
+    "revision": "a81e87970a5c36defcfe",
+    "url": "/smart-brain/static/js/2.2203b2e1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smart-brain/static/js/2.2203b2e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "703f63b4d5f87cdd2f28",
+    "url": "/smart-brain/static/js/main.d11e1049.chunk.js"
+  },
+  {
+    "revision": "18e194505f8cc7c2ec2c",
+    "url": "/smart-brain/static/js/runtime-main.16d42c62.js"
+  }
+]);
